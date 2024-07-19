43995966aae90def6ef58cc2a396f979759d9ee0
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-07-20 00:45:56 +0300

    imports

--- lib/absolute_package/package1/module1.py
+++ lib/absolute_package/package1/module1.py
@@ -1,2 +1,6 @@
 def function1():
     print('Function 1 in module 1')
+
+
+
+#An absolute import specifies the module to be imported using the full path from the project root directory.
---
+++ lib/relative_package/__init__.py
@@ -0,0 +1,5 @@
+
+"""
+#The reason why relative imports exist is because they allow us to rearrange the structure of large packages without having to edit sub-packages. 
+If we rearrange a large package with absolute imports, we have to change all the paths in every file's import statements to match the new location.
+"""
